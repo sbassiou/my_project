e082e71301547343f58172512358bbd14a66c0cd
Sarah Bassiouni <sbassiou@umich.edu>
2016-03-18 23:36:27 -0400

    Assignment, part 2

--- 612.1_AssignmentScript.R
+++ 612.1_AssignmentScript.R
@@ -11,11 +11,24 @@ colnames(gapminder)
 #Familiarizing self with gapminder
 str(gapminder)
 
+
+#Creating plot, part 1.
+library("ggplot2")
+
+#Creating plot, part 2.
+ggplot(data = gapminder, aes(x =year, y =lifeExp, color=continent)) +
+  geom_point() + geom_smooth(method="lm") +
+  xlab("Year") + ylab("Life expectancy") + ggtitle("Figure 1. Life expectancy from 1952 to 2007") +
+  scale_colour_discrete(name="Continent") +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+
+#Code not needed; for practice
 calcGDP <- function(dat) {
   gdp <- dat$pop * dat$gdpPercap
   return(gdp)
 }
 
+#Code not needed; for practice
 calcGDP <- function(dat, year=NULL, country=NULL) {
   if(!is.null(year)) {
     dat <- dat[dat$year %in% year, ]
@@ -28,12 +41,3 @@ calcGDP <- function(dat, year=NULL, country=NULL) {
   new <- cbind(dat, gdp=gdp)
   return(new)
 }
-
-library("ggplot2")
-
-ggplot(data = gapminder, aes(x =year, y =lifeExp, color=continent)) +
-  geom_point() + geom_smooth(method="lm") +
-  xlab("Year") + ylab("Life expectancy") + ggtitle("Figure 1. Life expectancy from 1952 to 2007") +
-  scale_colour_discrete(name="Continent") +
-  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
-
